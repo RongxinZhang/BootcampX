a5faf2d0d3e0d341cd591fe977f2ee6b6e0c0968
Rongxin Zhang <mrrongxin@gmail.com>
2020-08-18 13:55:14 -0700

    Add 2_queries_joins

--- 2_queries_joins/01_student_time_spent.sql
+++ 2_queries_joins/01_student_time_spent.sql
@@ -1,1 +1,6 @@
-SELECT * 
+select SUM(assignment_submissions.duration) as total_duration
+FROM assignment_submissions
+join students
+on student_id = students.id
+where students.name = 'Ibrahim Schimmel';
+
